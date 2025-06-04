78685c1512819028306e0e82214a50de119ddda6
AngelloVasconez <127260680+Alvasconv@users.noreply.github.com>
2025-06-04 02:51:58 +0000

    Cambios de la guia 7

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+import 'flowbite';
 
 document.querySelector('#app').innerHTML = `
   <div>
